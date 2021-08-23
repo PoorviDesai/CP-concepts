ca09001de04151ab66a1bd156776ba496efd5aa0
Poorvi Desai <81794133+PoorviDesai@users.noreply.github.com>
2021-08-23 23:46:48 +0530

    Create Primality Test.cpp
    to determine whether the input int is prime or not

---
+++ Fundamentals/Primality Test.cpp
@@ -0,0 +1,3 @@
+The naive approach requires time complexity of O(n)....but using the property:
+all divisors of a prime number N occur in pairs of (a, b) such that a * b = N..
+so the time complexity becomes O(sqrt(n))
